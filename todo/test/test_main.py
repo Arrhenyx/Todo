8b9d4d77258ee2b4e42bb63646f44eca1c8a25f1
fatemefaraji <f.faraji54@umail.umz.ac.ir>
2026-05-24 11:57:10 +0330

    statst writing tests

--- todo/test/test_main.py
+++ todo/test/test_main.py
@@ -5,7 +5,7 @@ from fastapi import status
 
 client = TestClient(main.app)
 
-
+# start writing tests
 def test_return_health_check():
     response = client.get("/healthy")
     assert response.status_code == status.HTTP_200_OK
